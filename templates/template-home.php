c3f8e02b8200412426b18d611a691d22891fbe25
jawittdesigns <Killjoy11>
2015-12-02 21:28:12 -0500

    Added home template

--- templates/template-home.php
+++ templates/template-home.php
@@ -3,7 +3,5 @@
 Template Name: Home
 */
 get_header();
-get_template_part( 'template-parts/homepage/ales' );
-get_template_part( 'template-parts/homepage/people' );
-get_template_part( 'template-parts/homepage/events' );
-get_footer(); ?>
+?>
+<?php get_footer(); ?>
