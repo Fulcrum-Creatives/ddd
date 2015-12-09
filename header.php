3e06876d2a398683a21506a69ac51ef757a71239
jawittdesigns <Killjoy11>
2015-12-09 16:34:23 -0500

    Changed header color

--- header.php
+++ header.php
@@ -1,5 +1,11 @@
 <?php get_template_part( 'template-parts/head' ); ?>
-<header id="header" class="header" role="banner">
+<?php 
+$green = '';
+if( is_page('case-studies') || is_single() ) :
+  $green = ' header__green';
+endif;
+?>
+<header id="header" class="header<?php echo $green; ?>" role="banner">
   <div class="content__wrapper">
     <div id="header__logo" class="header__logo">
       <?php get_template_part( 'template-parts/partials/logo' ); ?>
