82ae017dc6988d7894f09f835e5e3593815b2c08
jawittdesigns <Killjoy11>
2015-12-09 16:40:46 -0500

    added conditional

--- template-parts/partials/case-studies-button.php
+++ template-parts/partials/case-studies-button.php
@@ -1,9 +1,11 @@
 <?php
+$count = wp_count_posts( 'post' );
 if ( have_posts() ) : 
   while ( have_posts() ) : 
     the_post();
     $ddd_btn_case_studies      = dfw_get_field( 'ddd_btn_case_studies' );
     $ddd_btn_case_studies_link = dfw_get_field( 'ddd_btn_case_studies_link' );
+    if( $count->publish >= 5 ) :
 ?>
     <div class="button__one centered">
       <a href="<?php echo $ddd_btn_case_studies_link; ?>">
@@ -11,6 +13,7 @@ if ( have_posts() ) :
       </a>
     </div>
 <?php
+    endif;
   endwhile; 
 endif;
 wp_reset_postdata();
