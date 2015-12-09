403f4babcb87041e75cdee9345a5b134b05174a8
jawittdesigns <Killjoy11>
2015-12-09 16:23:21 -0500

    removed header title

--- header.php
+++ header.php
@@ -4,11 +4,6 @@
     <div id="header__logo" class="header__logo">
       <?php get_template_part( 'template-parts/partials/logo' ); ?>
     </div>
-    <?php 
-    if( !is_front_page() ) :
-       if( function_exists( 'dfw_page_title' ) ) : dfw_page_title(); endif;
-    endif;
-    ?>
   </div>
 </header>
 <main id="main" class="main" role="main">
