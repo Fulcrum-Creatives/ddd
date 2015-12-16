7103cc3dab92d20192a660a08c03ed788a3630b9
jawittdesigns <Killjoy11>
2015-12-16 13:02:57 -0500

    Added news title and date to news single

--- template-parts/entry/single.php
+++ template-parts/entry/single.php
@@ -4,7 +4,13 @@ $ddd_cs_sub_tilte_one = dfw_get_field( 'ddd_cs_subtilte_one' );
 $ddd_cs_subtitle_two  = dfw_get_field( 'ddd_cs_subtitle_two' );
 ?>
 <div class="content__wrapper top">
-  <h2 class="case-studies__subtitle one"><?php echo $ddd_cs_sub_tilte_one; ?></h2>
-  <h2 class="case-studies__subtitle two"><?php echo $ddd_cs_subtitle_two; ?></h2>
+  
+  <?php if( is_singular( 'casestudies' ) ) : ?>
+    <h2 class="case-studies__subtitle one"><?php echo $ddd_cs_sub_tilte_one; ?></h2>
+    <h2 class="case-studies__subtitle two"><?php echo $ddd_cs_subtitle_two; ?></h2>
+  <?php else: ?>
+    <?php if( function_exists( 'dfw_entry_title' ) ) : dfw_entry_title( array( 'has_link' => false )); endif; ?>
+    <div class="entry__date"><?php echo get_the_date(); ?></div>
+  <?php endif; ?>
   <?php the_content(); ?>
 </div>
