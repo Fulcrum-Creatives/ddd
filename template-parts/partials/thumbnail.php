e8ee284e4269481b5613aa3c4d0d1e8e8ac5c6d8
jawittdesigns <Killjoy11>
2015-12-16 11:55:01 -0500

    Added caption to homepage thumbnails

--- template-parts/partials/thumbnail.php
+++ template-parts/partials/thumbnail.php
@@ -1,5 +1,8 @@
 <?php if ( has_post_thumbnail() ) : ?>
   <div class="entry__thumbnail">
     <?php the_post_thumbnail();?>
+    <p class="wp-caption-text">
+      <?php echo get_post(get_post_thumbnail_id())->post_excerpt; ?>
+    </p>
   </div>
 <?php endif ?>
